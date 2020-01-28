04569e137b54af661f93c7d050a5d6d20e6d10e8
Pasante <Pasante@CSID.LOCAL>
2020-01-28 16:52:42 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number);
+            number = 20;
+            Console.WriteLine(number);
+            Console.WriteLine("Nayeli");
+            Console.WriteLine(18);
+            Console.Read();
+        }
+    }
+}
